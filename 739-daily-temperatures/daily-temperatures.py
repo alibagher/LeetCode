1f74c4c8453701103bed1c051ddcd8a22752dc57
Ali Bagheri <a6bagher@uwaterloo.ca>
2024-05-21 15:59:22 -0400

    Time: 1406 ms (5.01%) | Memory: 30.9 MB (62.24%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []
+        for i, t in enumerate(temperatures):
+            print (t, i)
+            if stack:
+                while stack and (stack[-1])[0] < t:
+                    print (stack[-1][1])
+                    res[(stack[-1])[1]] = i - (stack[-1])[1]
+                    stack.pop()
+            stack.append((t,i))
+        return res
